3808b87e37929d113ed5253a5ef21c37123b18cd
ludoguenet <ludovicguenet@gmx.com>
2023-04-28 15:40:14 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <HelloWorld
+      @update-name="name => display(name)"
+    />
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+const display = (name) => {
+    console.log('App.vue : ' + name);
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello World</h1>
+
+    {{ name }}
+
+    <input type="text" v-model="name" @input="handleUpdate">
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['updateName']);
+
+const handleUpdate = (event) => {
+    emit('updateName', event.target.value);
+}
+
+const name = ref('');
+</script>
